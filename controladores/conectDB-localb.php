f2ad6f0fe3b2d80b8c7114dc0c64a447ccfa69a0
Javier Castorena <kastor.kruel@gmail.com>
2021-11-19 08:21:36 -0800

    112

--- controladores/conectDB-localb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "financierachavez";
-$username = "fuser";
-$pass = "1234";
-$conectionInfo = array("Database" => $database, "UID" => $username, "PWD" => $pass, 'CharacterSet' => 'UTF-8');
-$con = sqlsrv_connect($servername, $conectionInfo);
-?>
