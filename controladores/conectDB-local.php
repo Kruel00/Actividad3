14c6d05b3d7f6c66d85767d9f75d30854dcadacb
Javier Castorena <kastor.kruel@gmail.com>
2021-11-25 16:30:47 -0800

    sds

--- controladores/conectDB-local.php
+++ controladores/conectDB-local.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "SQL5102.site4now.net";
-$database = "db_a7cb02_financierachavez";
-$username = "db_a7cb02_financierachavez_admin";
+$servername = "localhost";
+$database = "FUS";
+$username = "jcastorena";
 $pass = "Cast0rena!@#";
 $conectionInfo = array("Database" => $database, "UID" => $username, "PWD" => $pass, 'CharacterSet' => 'UTF-8');
 $con = sqlsrv_connect($servername, $conectionInfo);
